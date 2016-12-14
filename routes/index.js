e95fa5519355283e1d043cd61659ee2076500d6b
keisuke makino <mknkisk@gmail.com>
2016-12-14 18:56:09 +0900

    :tada: Logout feature

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,10 @@ router.get('/', function (req, res, next) {
   })
 })
 
+router.get('/logout', function (req, res, next) {
+  req.logout()
+  req.session.destroy()
+  res.redirect('/')
+})
+
 module.exports = router
