5e5fa302d1b32190b75409416caeb37fe5b5000a
Keisuke Makino <mknkisk@gmail.com>
2017-01-09 00:00:55 +0900

    :shower: Remove config/db.js, not used

--- config/db.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  production: 'mongodb://user:pass@example.com:1234/awesome_events_production',
-  development: 'mongodb://localhost:27017/awesome_events_development',
-  test: 'mongodb://localhost:27017/awesome_events_test'
-};
